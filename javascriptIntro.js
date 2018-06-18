8042c69bda6780592f67162adc602139d4f3aff7
Mário Gil <mg_mesquita@hotmail.com>
2018-06-18 16:04:13 +0100

    Javascript Intro

---
+++ javascriptIntro.js
@@ -0,0 +1,11 @@
+/*var firstName = prompt("First name?");
+var lastName = prompt("Last name?");
+var age = prompt("Age?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("Your age is " + age);*/
+
+var age = prompt("Age?");
+var daysAlive = Number(age) * 365;
+
+alert("You have been alive for " + String(daysAlive) + " days.");
