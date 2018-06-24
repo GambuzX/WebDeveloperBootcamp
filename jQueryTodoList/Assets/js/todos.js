452a6b2c7e949aaaf19b0959a9ad6038001b5413
Mário Gil <mg_mesquita@hotmail.com>
2018-06-24 15:13:39 +0100

    Adding todos

--- jQueryTodoList/Assets/js/todos.js
+++ jQueryTodoList/Assets/js/todos.js
@@ -1,10 +1,21 @@
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("checked");
 })
 
-$("li span").on("click", function(event){
+$("ul").on("click", "li span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+})
+
+$("input[type='text']").on("keypress", function(event){
+	if (event.which === 13) {
+		//Extract text
+		var textContent = $(this).val();
+		//Make new li and add to ul
+		$("ul").append("<li><span>X</span> " + textContent + "</li>");
+		//Remove previous text
+		$(this).val("");
+	}
 })
